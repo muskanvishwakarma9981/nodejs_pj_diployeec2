8c98b2ed6f5ea4d44c76260cba817186da1e51c6
muskanvishwakarma9981 <muskanvishwakarma9981@gmail.com>
2024-08-29 12:53:02 +0530

    get user details

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/api/getapi', (req, res)=>{
     res.send({message:"hello get api"})
 })
 
+app.get('/api/get_user_details', (req, res)=>{
+    res.send({
+        user:{
+            name:"muskan",
+            age:23,
+            contact:12344
+        }
+    })
+})
+
 app.listen(8000, ()=>{
     console.log('serevre is running')
 })
